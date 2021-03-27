e084299ecf7b2d2297873b8c3f2f23c0041924bb
superColin <supercolin.dev@gmail.com>
2021-03-27 14:17:20 -0400

    added some tests, started on form validation

--- src/components/AddStayForm.js
+++ src/components/AddStayForm.js
@@ -1,39 +1,83 @@
-
+import {useState} from 'react';
 
 const AddStayForm = () => {
-    const handler = (e)=>{
+    const [groupName, setGroupName]= useState('');
+    const [petName, setPetName]= useState('');
+    const [arrivalDate, setArrivalDate] = useState('');
+    const [arrivalTime, setArrivalTime] = useState('');
+    const [releaseDate, setReleaseDate] = useState('');
+    const [releaseTime, setReleaseTime] = useState('');
+    const [petNotes,setPetNotes] = useState('');
+
+    const validateInput = (validationCallback) =>{
+        const validationOutput = validationCallback();
+        const errorOutputElem = document.getElementById('addStay-errorReport');
+        if( validationOutput !== 'valid' ){
+            errorOutputElem.style.display = 'block';
+            errorOutputElem.innerHTML=validationOutput + '!';            
+        } else{
+            errorOutputElem.style.display = 'none';
+        }
+    };
+
+    const groupNameValidation = ()=>{
+        let groupNameElem = document.getElementById('addStay-groupName');
+        if(groupName.length === 0 || groupName.length < 4){
+            groupNameElem.classList.add('invalidInput');
+            groupNameElem.setAttribute("isvalid", "false");
+            return 'Please use a group name with at least 4 letters';
+        }else{
+            groupNameElem.classList.remove('invalidInput');
+            groupNameElem.setAttribute("isvalid", "true")
+            return 'valid'
+        }
+    }
+
+    const submitHandler = (e)=>{
         e.preventDefault();
+        validateInput(groupNameValidation);
+        
         console.log('form input is:', e)
+        console.log('pet name is', petName);
+        console.log('pet name length is', petName.length);
+        console.log('arrival date is', arrivalDate);
     }
     return (
-        <form className="addStayForm_container" onSubmit={handler}>
+        <form className="addStayForm_container" onSubmit={submitHandler}>
             <h1>Add Stay</h1>
+            <label htmlFor="addStay-groupName">Group Name</label>
+            <input id="addStay-groupName" type="text" value={groupName} onChange={(e)=> setGroupName(e.target.value)} />
+            <br />
+            <br />
+
             <label htmlFor="addStay-petName">Pet Name</label>
-            <input id="addStay-petName" type="text" />
+            <input id="addStay-petName" type="text" value={petName} onChange={(e)=> setPetName(e.target.value)} />
             <br />
             <br />
 
             <label htmlFor="addStay-arrivalDate">Arrival Date</label>
-            <input id="addStay-arrivalDate" type="date" />
+            <input id="addStay-arrivalDate" type="date" value={arrivalDate} onChange={(e)=> setArrivalDate(e.target.value)} />
 
             <label htmlFor="addStay-arrivalTime">Arrival Time</label>
             {/* <input id="addStay-arrivalTime" type="number" step="0.5" min="0" max="24" /> */}
-            <input id="addStay-arrivalTime" type="time" />
+            <input id="addStay-arrivalTime" type="time" value={arrivalTime} onChange={(e)=> setArrivalTime(e.target.value)} />
             <br />
             <br />
 
             <label htmlFor="addStay-releaseDate">Release Date</label>
-            <input id="addStay-releaseDate" type="date" />
+            <input id="addStay-releaseDate" type="date"  value={releaseDate} onChange={(e)=> setReleaseDate(e.target.value)}  />
 
             <label htmlFor="addStay-releaseTime">Release Time</label>
-            <input id="addStay-releaseTime" type="time" />
+            <input id="addStay-releaseTime" type="time" value={releaseTime} onChange={(e)=> setReleaseTime(e.target.value)} />
             <br />
             <br />
 
             <label htmlFor="addStay-petNotes">Pet Notes</label>
             <br />
-            <textarea id="addStay-petNotes" placeholder="Notes, medical or otherwise" />
+            <textarea id="addStay-petNotes" placeholder="Notes, medical or otherwise" value={petNotes} onChange={(e)=> setPetNotes(e.target.value)} />
             {/* <input id="addStay-petNotes" type="textarea" /> */}
+            <br />
+            <span style={{"display":"none"}} id="addStay-errorReport"></span>
             <button formAction="submit">Submit</button>
         </form>
     )
--- src/components/__tests__/addStayForm.test.js
+++ src/components/__tests__/addStayForm.test.js
@@ -1,24 +1,59 @@
 import {screen, render} from '@testing-library/react';
-// import renderer from 'react-test-renderer';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
+
 import AddStayForm from '../AddStayForm';
 
+describe('Suite of tests for the AddStayForm component',()=>{
 
-test('See if AddStayForm exists',()=>{
-    render(<AddStayForm />);
-    const form = document.querySelector('.addStayForm_container');
-    expect(form).toBeInTheDocument();
-    expect(form).toHaveTextContent('Add Stay');
-});
 
-test('Check that AddStayForm has approriate inputs', ()=>{
-    render(<AddStayForm />);
-    const nameInput = screen.getByLabelText('Pet Name');
-    const arrivalDateInput = screen.getByLabelText('Arrival Date');
-    const arrivalTimeInput = screen.getByLabelText('Arrival Time');
-    const releaseDateInput = screen.getByLabelText('Release Date');
-    const releaseTimeInput = screen.getByLabelText('Release Time');
-    const notesInput = screen.getByLabelText('Pet Notes');
-    const formSubmitButton = screen.getByText('Submit');
+    test('See if AddStayForm exists',()=>{
+        render(<AddStayForm />);
+        const form = document.querySelector('.addStayForm_container');
+        expect(form).toBeInTheDocument();
+        // expect(form).toHaveTextContent('Add Stay');
+    });
+
+    test('Check that AddStayForm has approriate inputs', ()=>{
+        render(<AddStayForm />);
+        const nameInput = screen.getByLabelText('Pet Name');
+        const arrivalDateInput = screen.getByLabelText('Arrival Date');
+        const arrivalTimeInput = screen.getByLabelText('Arrival Time');
+        const releaseDateInput = screen.getByLabelText('Release Date');
+        const releaseTimeInput = screen.getByLabelText('Release Time');
+        const notesInput = screen.getByLabelText('Pet Notes');
+        const formSubmitButton = screen.getByRole('button', {"name":"Submit"} );
+    });
+
+    test.only('Check that form can accept input and submit', ()=>{
+        render(<AddStayForm />);
+        let i = 0;
+        const stringInputs = [
+            screen.getByLabelText('Pet Name'),
+            screen.getByLabelText('Pet Notes') ];
+
+        const dateInputs = [
+            screen.getByLabelText('Arrival Date'),
+            screen.getByLabelText('Release Date') ];
+
+        const timeInputs = [
+            screen.getByLabelText('Arrival Time'),
+            screen.getByLabelText('Release Time') ];
+
+        const submitButton = screen.getByRole('button', {"name":"Submit"} );
+
+        stringInputs.map( input => {
+            userEvent.type(input, `name ${i}`);
+            expect(input).toHaveValue(`name ${i}`);
+            i++;
+        });
+        dateInputs.map( input =>{
+            
+        });
+        // userEvent.type(nameInput, "Mr. Kitty");
+        // expect(nameInput).toHaveValue('Mr. Kitty');
+    });
+
+
 });
 
