e6ed358be8442caa646dfe2cbb191778a9658763
superColin <supercolin.dev@gmail.com>
2021-04-03 11:54:58 -0400

    added inputs and remove pet functionality

--- src/components/AddStayForm.js
+++ src/components/AddStayForm.js
@@ -1,33 +1,17 @@
 import {useState} from 'react';
 
 const AddStayForm = ({passNewGroupStayUpScope}) => {
-    const [groupName, setGroupName]= useState('Group from form');
-    const [petName, setPetName]= useState('Mr. Kitty');
-    const [arrivalDate, setArrivalDate] = useState('2021/03/16');
-    const [arrivalTime, setArrivalTime] = useState('09:15');
-    const [releaseDate, setReleaseDate] = useState('2021/03/19');
-    const [releaseTime, setReleaseTime] = useState('17:00');
-    const [petNotes,setPetNotes] = useState('heres some notes');
-    const [kennelSize,setKennelSize] = useState('medium');
-        const boilerplatePetDetails = {
-            // "formKey": 1,2,3..
-            "petName": "Mr. Kitty",
-            "kennelSize": 'medium',
-            "notes":'notes note notes etNote',
-            "arrivalDate": '2021/03/16',
-            "arrivalTime": '17:00',
-            "releaseDate": '2021/03/19',
-            "releaseTime": '17:00',
-            "singleDayStay": false
-        }
-        
-    const increasePetStayInputs = ()=>{
-        let newState = { ...addStayFormStayState};
-        let newPetStayLabel = Object.keys(newState.pets).length * Math.ceil(Math.random() * 35);
-        newState.pets[newPetStayLabel] = {"formKey":newPetStayLabel , ...boilerplatePetDetails}
-        setAddStayFormStayState(newState);
+    const boilerplatePetDetails = {
+        // "formKey": 1,2,3..
+        "petName": "Mr. Kitty",
+        "kennelSize": 'medium',
+        "notes":'note note note',
+        "arrivalDate": '2021/03/16',
+        "arrivalTime": '17:00',
+        "releaseDate": '2021/03/19',
+        "releaseTime": '17:00',
+        "singleDayStay": false
     }
-
     const [addStayFormStayState, setAddStayFormStayState] = useState({
         "groupName": "Smith",
         "groupNotes": "Here's some notes",
@@ -36,21 +20,28 @@ const AddStayForm = ({passNewGroupStayUpScope}) => {
                 ...boilerplatePetDetails
             },
         }
-
     })
-
-
     const updateGroupState = (e)=>{
         setAddStayFormStayState({ ...addStayFormStayState, [e.target.name]: e.target.value})
     }
     const updatePetState = (e)=>{
-        // console.log('previous state is : ', addStayFormStayState)
         let newState = { ...addStayFormStayState};
-        // console.log('newState is : ', newState)
         newState.pets[e.target.getAttribute('data-formkey')] = {...newState.pets[e.target.getAttribute('data-formKey')], [e.target.name]:e.target.value }
         setAddStayFormStayState(newState);
     }
-
+    const increasePetStayInputs = ()=>{
+        let newState = { ...addStayFormStayState};
+        let newPetStayLabel = Math.ceil((Object.keys(newState.pets).length * 2) * Math.random() * 1000);
+        newState.pets[newPetStayLabel] = {"formKey":newPetStayLabel , ...boilerplatePetDetails}
+        setAddStayFormStayState(newState);
+    }
+    const removePetStayInput = (e)=>{
+        let newState = { ...addStayFormStayState};
+        let key = e.target.getAttribute('data-formkey');
+        // console.log('remove input key:', key);
+        delete newState.pets[key];
+        setAddStayFormStayState(newState);
+    }
     const addStaySubmitHandler = (e)=>{
         e.preventDefault();
         const petsInStay = [];
@@ -80,13 +71,50 @@ const AddStayForm = ({passNewGroupStayUpScope}) => {
 
             {Object.keys(addStayFormStayState.pets).map((petStayLabel)=>{
 
-                {console.log('petStay is : ', addStayFormStayState.pets[petStayLabel])}
+                {/* {console.log('petStay is : ', addStayFormStayState.pets[petStayLabel])} */}
                 return <div key={petStayLabel} className="addStayForm_petStay-container">
+                <button data-formkey={petStayLabel} onClick={(e)=>{removePetStayInput(e)}}>Remove</button>
                     <div className="addStayForm_inputWrapper">
                         <label htmlFor="addStay-petName">Pet Name</label>
                         <input required name="petName" data-formkey={petStayLabel} type="text" minLength="2" value={addStayFormStayState.pets[petStayLabel].petName} onChange={(e)=> updatePetState(e)} />
                     </div>
-                    
+
+                    <div className="addStayForm_inputWrapper">
+                        <label htmlFor="addStay-arrivalDate">Arrival Date</label>
+                        <input required name="arrivalDate" data-formkey={petStayLabel} type="date" data-formkey={petStayLabel} value={addStayFormStayState.pets[petStayLabel].arrivalDate} onChange={(e)=> updatePetState(e)} />
+
+                        <label htmlFor="addStay-arrivalTime">Arrival Time</label>
+                        <input required name="arrivalTime" data-formkey={petStayLabel} type="time" data-formkey={petStayLabel} value={addStayFormStayState.pets[petStayLabel].arrivalTime} onChange={(e)=> updatePetState(e)} />
+                    </div>
+                    <br />
+
+                    <div className="addStayForm_inputWrapper">
+                        <label htmlFor="releaseDate">Release Date</label>
+                        <input required name="releaseDate" data-formkey={petStayLabel} type="date" value={addStayFormStayState.pets[petStayLabel].releaseDate} onChange={(e)=> updatePetState(e)}  />
+
+                        <label htmlFor="releaseTime">Release Time</label>
+                        <input required name="releaseTime" data-formkey={petStayLabel} type="time" value={addStayFormStayState.pets[petStayLabel].releaseTime} onChange={(e)=> updatePetState(e)} />
+                    </div>
+                    <br />
+
+                    <div className="addStayForm_inputWrapper">
+                        <label htmlFor="addStay-kennelSize">Kennel Size</label>
+                        <br />
+                        <select required name="kennelSize"  data-formkey={petStayLabel} value={addStayFormStayState.pets[petStayLabel].kennelSize} onChange={(e)=> updatePetState(e)} >
+                            <option value="small">Small</option>
+                            <option value="medium">Medium</option>
+                            <option value="large">Large</option>
+                        </select>
+                    </div>
+
+
+                    <div className="addStayForm_inputWrapper">
+                        <label htmlFor="addStay-petNotes">Pet Notes</label>
+                        <br />
+                        <textarea  name="notes" data-formkey={petStayLabel} value={addStayFormStayState.pets[petStayLabel].notes} onChange={(e)=> updatePetState(e)} placeholder="Notes, medical or otherwise" />
+                    </div>
+                    <br />
+
                 </div>
                 {/* /petStay form wrapper */}
             })}
